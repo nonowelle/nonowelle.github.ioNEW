4b842ec9f96e0a7d46230ad1230e10aeb9f6907a
nonowelle <menoel888@gmail.com>
2022-02-17 21:04:59 -0500

    add data

--- portfolio-2022/src/components/Projects.vue
+++ portfolio-2022/src/components/Projects.vue
@@ -4,38 +4,28 @@
     <div class="planties">
       <img
         class="projets-img show-on-scroll-left"
-        src="imgs/planties.png"
+        :src="projects[0].img"
         alt="captures-ecran-pages-web"
       />
       <div class="planties-texte show-on-scroll-right">
-        <h3>Planties</h3>
-        <p>
-          Planties est ma première application Web complète. Créez-vous un
-          compte et vous pourrez collectionner et partager des photos de vos
-          plantes avec d'autres passionnés. Elle est inspirée du projet Yelpcamp
-          du cours The Web Developper Bootcamp (par Colt Steele). Grâce à ce
-          projet, j'ai appris à utiliser Express, Node.js et MongoDB.
-        </p>
+        <h3>{{ projects[0].title }}</h3>
+        <p>{{ projects[0].description }}</p>
 
         <p>
           Technologies utilisées :
           <span class="tech"
-            >Bootstrap - Express - Node.js - MongoDB - EJS
-          </span>
+            ><li v-for="tech in projects[0].techs" :key="tech">
+              {{ tech }}
+            </li></span
+          >
         </p>
         <div class="btn">
-          <a
-            target="_blank"
-            href="https://serene-caverns-99636.herokuapp.com/"
-            class="site"
+          <a target="_blank" :href="projects[0].links[0]" class="site"
             >Site Web <i class="fas fa-long-arrow-alt-right lien"></i
           ></a>
         </div>
         <div class="btn" type="button">
-          <a
-            target="_blank"
-            href="https://github.com/nonowelle/planties"
-            class="site"
+          <a target="_blank" :href="projects[0].links[1]" class="site"
             >Code <i class="fas fa-long-arrow-alt-right lien"></i
           ></a>
         </div>
@@ -43,75 +33,59 @@
     </div>
     <div class="crism">
       <div class="crism-texte show-on-scroll-left">
-        <h3>CRISM</h3>
+        <h3>{{ projects[1].title }}</h3>
         <p>
-          À titre d'édimestre pour l'<em
-            >Initiative canadienne de recherche en abus de substances </em
-          >(CRISM), j'avais pour mandat de procéder à la refonte du visuel et du
-          contenu du site Web de l'organisation. Je devais utiliser le thème
-          Wordpress mis en place lors de la création du site, Jupiter 6. J'ai
-          donc créé une nouvelle arborescence, établi une nouvelle palette de
-          couleurs et créé des maquettes avec Figma pour finalement mettre en
-          place le nouveau design. J'ai également procédé à l'ajout de
-          métadecriptions via le plugin Yoast pour améliorer le référencement
-          naturel.
+          {{ projects[1].description }}
         </p>
         <p>
           Technologies utilisées :
           <span class="tech"
-            >Wordpress - Thème Jupiter 6 - CSS - YoastSEO - Figma
+            ><li v-for="tech in projects[1].techs" :key="tech">
+              {{ tech }}
+            </li>
           </span>
         </p>
         <div class="btn">
-          <a
-            target="_blank"
-            href="https://www.crismquebecatlantic.ca/"
-            class="site"
+          <a target="_blank" :href="projects[1].links[0]" class="site"
             >Site Web <i class="fas fa-long-arrow-alt-right lien"></i
           ></a>
         </div>
       </div>
       <img
         class="show-on-scroll-right projets-img show-on-scroll"
-        src="imgs/CRISM.png"
+        :src="projects[1].img"
         alt="captures-ecran-pages-web"
       />
     </div>
 
     <div class="landing">
       <div class="landing-texte show-on-scroll-right">
-        <h3>Page FAQ</h3>
+        <h3>{{ projects[2].title }}</h3>
         <p>
-          Ce projet constitue mon troisième défi de Front-End Mentor Challenge.
-          Il s'agit d'une section <em>Questions fréquentes</em> sous forme
-          d'accordéon. Je devais reproduire le plus fidèlement possible le
-          design qui m'avait été fourni en utilisant les outils de mon choix.
-          Mon design devait être responsive et s'adapter à tous les navigateurs.
+          {{ projects[2].description }}
         </p>
         <p>
           Technologies utilisées :
-          <span class="tech">HTML - CSS - Vanilla JS </span>
+          <span class="tech"
+            ><li v-for="tech in projects[2].techs" :key="tech">
+              {{ tech }}
+            </li></span
+          >
         </p>
         <div class="btn">
-          <a
-            target="_blank"
-            href="https://faq-accordion-card-challenge-git-main.nonowelle.vercel.app/"
-            class="site"
+          <a target="_blank" :href="projects[2].links[0]" class="site"
             >Site Web <i class="fas fa-long-arrow-alt-right lien"></i
           ></a>
         </div>
         <div class="btn" type="button">
-          <a
-            target="_blank"
-            href="https://github.com/nonowelle/faq-accordion-card-challenge"
-            class="site"
+          <a target="_blank" :href="projects[2].links[0]" class="site"
             >Code <i class="fas fa-long-arrow-alt-right lien"></i
           ></a>
         </div>
       </div>
       <img
         class="projets-img show-on-scroll-left"
-        src="/imgs/accordion.png"
+        :src="projects[2].img"
         alt=""
       />
     </div>
@@ -121,5 +95,41 @@
 <script>
 export default {
   name: "Projects",
+  data: function () {
+    return {
+      projects: [
+        {
+          title: "Planties",
+          description:
+            "Planties est ma première application Web complète. Créez-vous un compte et vous pourrez collectionner et partager des photos de vos plantes avec d'autres passionnés. Elle est inspirée du projet Yelpcamp du cours The Web Developper Bootcamp (par Colt Steele). Grâce à ce projet, j'ai appris à utiliser Express, Node.js et MongoDB.",
+          img: "imgs/planties.png",
+          techs: ["Bootstrap", "Express", "Node.js", "MongoDB", "EJS"],
+          links: [
+            "https://serene-caverns-99636.herokuapp.com/",
+            "https://github.com/nonowelle/planties",
+          ],
+        },
+        {
+          title: "CRISM",
+          description:
+            "À titre d'édimestre pour l'<em>Initiative canadienne de recherche en abus de substances </em>(CRISM), j'avais pour mandat de procéder à la refonte du visuel et du contenu du site Web de l'organisation. Je devais utiliser le thème Wordpress mis en place lors de la création du site, Jupiter 6. J'ai donc créé une nouvelle arborescence, établi une nouvelle palette de couleurs et créé des maquettes avec Figma pour finalement mettre en place le nouveau design. J'ai également procédé à l'ajout de métadecriptions via le plugin Yoast pour améliorer le référencement naturel.",
+          img: "imgs/CRISM.png",
+          techs: ["Wordpress", "Thème Jupiter 6", "CSS", "YoastSEO", "Figma"],
+          links: ["https://www.crismquebecatlantic.ca/"],
+        },
+        {
+          title: "Page FAQ",
+          description:
+            "Ce projet constitue mon troisième défi de Front-End Mentor Challenge. Il s'agit d'une section <em>Questions fréquentes</em> sous forme d'accordéon. Je devais reproduire le plus fidèlement possible le design qui m'avait été fourni en utilisant les outils de mon choix. Mon design devait être responsive et s'adapter à tous les navigateurs.",
+          img: "/imgs/accordion.png",
+          techs: ["HTML", "CSS", "Vanilla JS"],
+          links: [
+            "https://faq-accordion-card-challenge-git-main.nonowelle.vercel.app/",
+            "https://github.com/nonowelle/faq-accordion-card-challenge",
+          ],
+        },
+      ],
+    };
+  },
 };
 </script>

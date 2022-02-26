655af64c96b7ebb8a3483f1304af09fee203ad63
nonowelle <menoel888@gmail.com>
2022-02-26 17:35:10 -0500

    fix section accueil
    Il reste le hero, la nav et le bas de page a refactorer en vue plus tout le css

--- portfolio-2022/src/components/Hero.vue
+++ portfolio-2022/src/components/Hero.vue
@@ -1,26 +1,28 @@
 <template>
-  <section class="accueil">
-    <div class="triangles show-on-scroll-right">
-      <div class="un"></div>
-      <div class="deux"></div>
-      <div class="trois"></div>
-      <div class="quatre"></div>
-      <div class="cinq"></div>
-    </div>
-    <div class="texte show-on-scroll-left">
-      <h1>
-        Bonjour! <br />
-        Moi, c'est <span class="nom">Marie-Ève</span>.
-      </h1>
-      <p class="description">
-        Je suis une développeuse front-end passionnée basée à Montréal et qui a
-        plus d’un tour dans son sac!
-      </p>
-    </div>
+  <div class="">
+    <section class="accueil">
+      <div class="triangles show-on-scroll-right">
+        <div class="un"></div>
+        <div class="deux"></div>
+        <div class="trois"></div>
+        <div class="quatre"></div>
+        <div class="cinq"></div>
+      </div>
+      <div class="texte show-on-scroll-left">
+        <h1>
+          Bonjour! <br />
+          Moi, c'est <span class="nom">Marie-Ève</span>.
+        </h1>
+        <p class="description">
+          Je suis une développeuse front-end passionnée basée à Montréal et qui
+          a plus d’un tour dans son sac!
+        </p>
+      </div>
+    </section>
     <a class="rond-fleche show-on-scroll-left" v-on:click="scrollToSection">
       <i class="fas fa-arrow-down fleche" ref="fleche"></i>
     </a>
-  </section>
+  </div>
 </template>
 
 <script>

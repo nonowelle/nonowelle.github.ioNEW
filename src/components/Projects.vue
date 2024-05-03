15a08893e7d27249b7cb6f4e26dd4b7c5b9ba4b5
marie-eve <menoel888@gmail.com>
2024-05-03 09:46:52 +0200

    arrows

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -50,9 +50,14 @@
 											:href="project.links[0]"
 											class="site"
 											>Site Web
-											<i
-												class="fas fa-long-arrow-alt-right lien"
-											></i
+											<svg
+												xmlns="http://www.w3.org/2000/svg"
+												viewBox="0 0 448 512"
+											>
+												<!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+												<path
+													d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"
+												/></svg
 										></a>
 									</div>
 									<div
@@ -64,10 +69,16 @@
 											:href="project.links[1]"
 											class="site"
 											>Code
-											<i
-												class="fas fa-long-arrow-alt-right lien"
-											></i
-										></a>
+											<svg
+												xmlns="http://www.w3.org/2000/svg"
+												viewBox="0 0 448 512"
+											>
+												<!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+												<path
+													d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"
+												/>
+											</svg>
+										</a>
 									</div>
 								</div>
 							</div>
